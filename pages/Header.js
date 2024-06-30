446e9bc9c03fa18b76fc9d7c870b4ac22e5b8f2a
ehcgnahc <jeter40906@gmail.com>
2024-07-01 01:08:26 +0800

    Header fixed

--- pages/Header.js
+++ pages/Header.js
@@ -6,29 +6,29 @@ function Header() {
         <header>
             <nav className={styles.menu}>
                 <ul>
-                    <li className={styles.hover}><Link href="/">Ehc-Code</Link></li>
+                    <li className={styles.hover}><Link href="/"><span className={styles.text}>Ehc-Code</span></Link></li>
                     <li>
-                        <Link href="/">MyCode<span className={styles.downArrow}></span></Link>
+                        <Link href="/"><span className={styles.text}>MyCode</span><span className={styles.downArrow}></span></Link>
                         <div className={styles.dropdown}>
                             <ul>
-                                <li><Link href="/Code-Cpp">C/Cpp</Link></li>
-                                <li><Link href="/">Python</Link></li>
-                                <li><Link href="/">My Website</Link></li>
-                                <li><Link href="/">Discord Bot</Link></li>
+                                <li><Link href="/Code-Cpp"><span className={styles.text}>C/Cpp</span></Link></li>
+                                <li><Link href="/"><span className={styles.text}>Python</span></Link></li>
+                                <li><Link href="/"><span className={styles.text}>My Website</span></Link></li>
+                                <li><Link href="/"><span className={styles.text}>Discord Bot</span></Link></li>
                             </ul>
                         </div>
                     </li>
                     <li>
-                        <Link href="/">About<span className={styles.downArrow}></span></Link>
+                        <Link href="/"><span className={styles.text}>About</span><span className={styles.downArrow}></span></Link>
                         <div className={styles.dropdown}>
                             <ul>
-                                <li><Link href="/About-the-Aurthor">About the Author</Link></li>
-                                <li><Link href="/">About the Website</Link></li>
+                                <li><Link href="/About-the-Aurthor"><span className={styles.text}>About the Author</span></Link></li>
+                                <li><Link href="/"><span className={styles.text}>About the Website</span></Link></li>
                             </ul>
                         </div>
                     </li>
-                    <li><a href="/">Services</a></li>
-                    <li><Link href="/Contact">Contact</Link></li>
+                    <li className={styles.hover}><Link href="/"><span className={styles.text}>Services</span></Link></li>
+                    <li className={styles.hover}><Link href="/Contact"><span className={styles.text}>Contact</span></Link></li>
                 </ul>
             </nav>
         </header>
