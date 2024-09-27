5ab9f26e07675909b15a97311523a4ef53ba283a
ehcgnahc <jeter40906@gmail.com>
2024-09-27 07:16:20 +0000

    carousel arrow building

--- pages/components/Carousel.js
+++ pages/components/Carousel.js
@@ -1,18 +1,25 @@
 import React from "react";
-import Styles from "../../styles/Carousel.module.css"
+import styles from "../../styles/Carousel.module.css"
 import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from "react-icons/bs"
 
 export default function Carousel({data}){
     console.log(data)
     return (
-        <div className={Styles.Carousel}>
-            <BsArrowLeftCircleFill/>
-            {data.map((item,idx)=>{
+        <div className={styles.Carousel}>
+            <BsArrowLeftCircleFill className={`${styles.arrow} ${styles.arrow-left}`}/>
+            {data["slides"].map((item,idx)=>{
                 return (
                     <img src={item.src} alt={item.alt} key={idx} className={Styles.slide}/>
                 );
             })}
-            <BsArrowRightCircleFill/>
+            <BsArrowRightCircleFill className={`${styles.arrow} ${styles.arrow-right}`}/>
+            <span className={styles.indicators}>
+                {
+                    data["slides"].map((_, idx)=>{
+                        return (<button key={idx} onClick={null}></button>);
+                    })
+                }
+            </span>
         </div>
     );
 };
