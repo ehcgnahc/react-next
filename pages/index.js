37c02c95dfe038871993bd09fc53bc6ccedff910
ehcgnahc <jeter40906@gmail.com>
2024-10-10 14:12:42 +0800

    a

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from '../styles/index.module.css';
 
 function Home({data}) {
-  const 
   return (
     <div className={styles.container}>
       <h1>Welcome to Ehc-Code</h1>
